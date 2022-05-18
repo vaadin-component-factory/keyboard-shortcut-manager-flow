aeebefb7c429bc579204e542251c651321ed6855
Nii Yeboah <nii@vaadin.com>
2022-05-18 21:16:00 +0300

    Change packaging

--- webpack.config.js
+++ webpack.config.js
@@ -5,33 +5,4 @@
 const merge = require('webpack-merge');
 const flowDefaults = require('./webpack.generated.js');
 
-module.exports = merge(flowDefaults, {
-
-});
-
-/**
- * This file can be used to configure the flow plugin defaults.
- * <code>
- *   // Add a custom plugin
- *   flowDefaults.plugins.push(new MyPlugin());
- *
- *   // Update the rules to also transpile `.mjs` files
- *   if (!flowDefaults.module.rules[0].test) {
- *     throw "Unexpected structure in generated webpack config";
- *   }
- *   flowDefaults.module.rules[0].test = /\.m?js$/
- *
- *   // Include a custom JS in the entry point in addition to generated-flow-imports.js
- *   if (typeof flowDefaults.entry.index != "string") {
- *     throw "Unexpected structure in generated webpack config";
- *   }
- *   flowDefaults.entry.index = [flowDefaults.entry.index, "myCustomFile.js"];
- * </code>
- * or add new configuration in the merge block.
- * <code>
- *   module.exports = merge(flowDefaults, {
- *     mode: 'development',
- *     devtool: 'inline-source-map'
- *   });
- * </code>
- */
+module.exports = merge(flowDefaults, {});
