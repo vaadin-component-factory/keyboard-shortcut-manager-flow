f711e83888517babda0f902970737a64faf3d011
Paola De Bartolo <paola.debartolo@gmail.com>
2026-01-22 15:35:37 -0300

    build(demo): add AppShellConfiguratorImpl class

---
+++ keyboard-shortcut-manager-flow-demo/src/main/java/org/vaadin/addons/componentfactory/AppShellConfiguratorImpl.java
@@ -0,0 +1,10 @@
+package org.vaadin.addons.componentfactory;
+
+import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.theme.lumo.Lumo;
+
+@StyleSheet(Lumo.STYLESHEET)
+public class AppShellConfiguratorImpl implements AppShellConfigurator {
+
+}
